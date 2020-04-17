ef9a0c30710e169e055b88b7c5721d76a8500d42
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-17 00:16:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,6 @@
 def prime?(num)
-  (2..(num - 1)).each do |n|
-    return false if num.abs % n == 0
-  end
-  if num == -1
-    false
+  (-1..2..(num - 1)).each do |n|
+    return false if num % n == 0
   end
   true
 end
