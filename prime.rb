666277f8392f514333224d65d1bb04b9a6584a82
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-17 00:21:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(num)
+  (2..(num - 1)).each do |n|
+    return false if num % n == 0
+  end
+  return false if num == -1
+  return false if num == 0
+  return false if num == 1
+  true
+end
